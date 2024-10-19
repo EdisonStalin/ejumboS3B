5e231101929541d9a4eb3b2b6b60e05d13731b93
master <master@SMLAB306PC03_25>
2024-10-18 19:37:52 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ejumboS3B
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
